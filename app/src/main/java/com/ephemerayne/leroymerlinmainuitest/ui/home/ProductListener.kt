a835839bdde1b9e9901f23b42fb77896f0e85f41
ephemerayne <nika_landers@mail.ru>
2021-04-22 15:12:30 +0300

    ripple foreground for category and product cardview added

--- app/src/main/java/com/ephemerayne/leroymerlinmainuitest/ui/home/ProductListener.kt
+++ app/src/main/java/com/ephemerayne/leroymerlinmainuitest/ui/home/ProductListener.kt
@@ -3,5 +3,6 @@ package com.ephemerayne.leroymerlinmainuitest.ui.home
 import com.ephemerayne.leroymerlinmainuitest.domain.entity.ProductEntity
 
 interface ProductListener {
+
     fun onProductClick(productEntity: ProductEntity)
 }
