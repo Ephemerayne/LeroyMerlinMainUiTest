3247359d9a048078f987627a89eeb717a2527b11
ephemerayne <nika_landers@mail.ru>
2021-04-23 19:43:14 +0300

    focus for search added

--- app/src/main/java/com/ephemerayne/leroymerlinmainuitest/ui/search/SearchFragment.kt
+++ app/src/main/java/com/ephemerayne/leroymerlinmainuitest/ui/search/SearchFragment.kt
@@ -1,11 +1,13 @@
 package com.ephemerayne.leroymerlinmainuitest.ui.search
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.animation.AnimationUtils
 import android.view.animation.LayoutAnimationController
+import android.view.inputmethod.InputMethodManager
 import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
 import com.ephemerayne.leroymerlinmainuitest.R
@@ -35,6 +37,11 @@ class SearchFragment : Fragment() {
         val controller: LayoutAnimationController =
             AnimationUtils.loadLayoutAnimation(context, R.anim.list_layout_controller)
         binding.searchList.layoutAnimation = controller
+
+        binding.toolbarLayout.search.requestFocus()
+        val inputManager =
+            activity?.getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+        inputManager?.showSoftInput(binding.toolbarLayout.search, InputMethodManager.SHOW_IMPLICIT)
     }
 }
 
